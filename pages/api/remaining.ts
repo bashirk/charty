e18a5bdc0a4b7edf82f29dec7b89c8e872c5e709
Korede <bashirkorede@gmail.com>
2024-04-09 01:41:48 +0100

    test

--- pages/api/remaining.ts
+++ pages/api/remaining.ts
@@ -7,6 +7,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const session = await getSession({ req });
+  console.log(session)
 
   if (!session || !session.user) {
     console.log('User not logged in');
